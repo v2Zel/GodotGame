d24f93bcf9043f28dcf895fd36e57025a2148042
CopperSheetMetal <shokihamanaka@students.freyberg.ac.nz>
2026-09-10 09:04:30 +1200

    Projectile
    Added projectiles the player can shoot

---
+++ scenes/projectile.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+var speed = 300
+var direction = 1 
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position.x += speed * direction*delta
--- scripts/character_body_2d.gd
+++ scripts/character_body_2d.gd
@@ -1,52 +1,61 @@
 extends CharacterBody2D
+
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var jump_sound: AudioStreamPlayer2D = $JumpSound
-
+@onready var bullet = preload("res://scenes/projectile.tscn")
+@onready var sprite: Sprite2D = $PotatoMan
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -850
+
 var alive = true
 
+
 func _physics_process(delta: float) -> void:
-	
+
 	if !alive:
 		return
-		
-	# Add animation 
+
+	# Shoot bullet
+	if Input.is_action_just_pressed("ui_accept"):
+		var bullet_temp = bullet.instantiate()
+		bullet_temp.direction = 1
+		add_child(bullet_temp)
+
+	# Add animation
 	if velocity.x > 1 or velocity.x < -1:
-		animated_sprite_2d.animation = "running" 
+		animated_sprite_2d.animation = "running"
 	else:
 		animated_sprite_2d.animation = "Idle"
-		 
-	# Add the gravity.
+
+	# Add the gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		animated_sprite_2d.animation = "jumping"
 
-	# Handle jump.
+	# Handle jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		jump_sound.play()
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Get the input direction
 	var direction := Input.get_axis("left", "right")
+
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
-	
+
+	# Flip sprite depending on direction
 	if direction == 1.0:
 		animated_sprite_2d.flip_h = false
 	elif direction == -1.0:
 		animated_sprite_2d.flip_h = true
-		
-@onready var sprite: Sprite2D = $PotatoMan
+
 
 func die() -> void:
-	#animated_sprite_2d.animation = "dying"
 	sprite.texture = preload("res://assets/images/Sprites/rock.png")
 	print("dies")
 	alive = false
